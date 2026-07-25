087fe176d3ee4d2de2974b2ac456a956ca74a934
Chelsea <chelsea.engert@gmail.com>
2026-07-25 15:00:25 -0400

    Increase performance on fonts

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,4 +1,5 @@
 "use client"; 
+export const dynamic = "force-static";
 import React, { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import ResumeContainer from './ResumeContainer';
